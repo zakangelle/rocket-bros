6736f165ea1300d23e71e9bb74cd93e30a0dc599
Zak Angelle <zaknotzak@gmail.com>
2016-09-10 12:06:06 -0500

    Collapsed Day 1 with expandable option

---
+++ main.js
@@ -0,0 +1,7 @@
+$('#day-1').on('show.bs.collapse', function() {
+  $('.see-hide-text').text('Hide Day 1 Results');
+});
+
+$('#day-1').on('hide.bs.collapse', function() {
+  $('.see-hide-text').text('See Day 1 Results');
+});
